65644575c91d3bcd1c740bb02db2ba6dd5429bf3
waleed677 <waleedmalik677@gmail.com>
2024-11-26 23:19:23 +0800

    update the main page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { FaTelegram, FaTimes } from 'react-icons/fa' // Importing icons from rea
 const Footer = () => {
   return (
     <div className="bg-black text-white p-6 text-center">
-      <p className="text-lg md:text-xl font-semibold">© COPYRIGHT PONDX</p>
+      <p className="text-lg md:text-xl font-semibold font-barcelona">© COPYRIGHT PONDX</p>
       <div className="max-w-xl mx-auto">
         <p className="text-sm md:text-sm mt-2 text-center">
           Pondx is a meme coin with no intrinsic value or financial expectations—purely for fun! Connect your wallet at app.uniswap.org using Metamask, paste the $PONDX address, and swap your SOLANA for Pondx.
